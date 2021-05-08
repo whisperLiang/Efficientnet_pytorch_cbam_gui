86b981e129df7567cfb052509b101de7256a21c5
whisperLiang <54267428+whisperLiang@users.noreply.github.com>
2021-05-08 21:02:15 +0800

    Add files via upload

--- utils/mymodel.py
+++ utils/mymodel.py
@@ -2,11 +2,16 @@ import torch.nn as nn
 from MyEfficientNet import cbam_EfficientNet
 from MyEfficientNet import EfficientNet
 # from utils import args
+import torch
 
+# b5 with cbam
 class Net_b5(nn.Module):
     def __init__(self, num_class):
         super(Net_b5, self).__init__()
+        # 导入融合cbam的Efficientnet模型
         self.model = cbam_EfficientNet.from_pretrained('efficientnet-b5', num_classes=1000)
+        # # 导入原始的Efficientnet模型
+        # self.model = EfficientNet.from_pretrained('efficientnet-b5', num_class=1000)
         self.num_class = num_class
         num_ftrs = self.model._fc.in_features
         self.fc = nn.Linear(num_ftrs,num_class)
@@ -16,7 +21,7 @@ class Net_b5(nn.Module):
         out = self.fc(out)
         return out
 
-
+# naive b5
 class Original_b5(nn.Module):
     def __init__(self, num_class):
         super(Original_b5, self).__init__()
@@ -30,12 +35,13 @@ class Original_b5(nn.Module):
         out = self.fc(out)
         return out
 
+# 多尺度融合模型b3 and b6
 class Net_multi_model(nn.Module):
     def __init__(self):
         super(Net_multi_model, self).__init__()
         
-        self.model1 = cbam_EfficientNet.from_pretrained('efficientnet-b5', num_classes=1000)
-        self.model2 = cbam_EfficientNet.from_pretrained('efficientnet-b2', num_classes=1000)
+        self.model1 = cbam_EfficientNet.from_pretrained('efficientnet-b6', num_classes=1000)
+        self.model2 = cbam_EfficientNet.from_pretrained('efficientnet-b3', num_classes=1000)
         num_ftrs = self.model1._fc.in_features + self.model2._fc.in_features
         # self.dropout = nn.Dropout(0.5)
         self.attention = nn.Sequential(nn.Linear(num_ftrs, num_ftrs//16),
